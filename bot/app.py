91043699c2502ecf7345b9320ed302e4c6fdd418
pujunru <pujunru@gmail.com>
2022-01-27 00:05:05 -0800

    Refactor the bot src to be compliant with the lambda invocation; Create github action to deploy serverless app on AWS

--- bot/app.py
+++ bot/app.py
@@ -2,15 +2,17 @@ import atexit
 import os
 from typing import Optional
 
-from peewee import SqliteDatabase
+from peewee import Database
 from slack_bolt import App as SlackBoltApp
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
 from config import SlackBotConfig
-from db.database import new_sqlite_database
+from db.database import conn_sqlite_database, conn_mysql_database
 from db.utils import init_db_if_not
 from middlewares import MiddlewareRegister, global_middlewares
 from listeners import ListenerRegister, listen_events, listen_commands, listen_messages
+from runtime import SlackBotRuntime
 
 
 class SlackBotApp:
@@ -20,12 +22,18 @@ class SlackBotApp:
         self.bolt_app = bolt_app  # Note DI for better testing
 
         self._socket_mode_handler: Optional[SocketModeHandler] = None
+        self._lambda_mode_handler: Optional[SlackRequestHandler] = None
 
         self.logger = None  # TODO!
 
         # Init database
-        self.db: SqliteDatabase = new_sqlite_database(config.db_name)
+        if not self.config.db_in_prod():
+            self.db: Database = conn_sqlite_database(config.db_name)
+        else:
+            self.db: Database = conn_mysql_database(config.db_url)
         # self.init_database()
+
+        self._runtime = SlackBotRuntime(self.db)
 
         # Register Slack middlewares
         self.register_middlewares(
@@ -42,7 +50,7 @@ class SlackBotApp:
     # Global registration of listeners for each event type/business category
     def register_listeners(self, *listener_register: ListenerRegister):
         for register in listener_register:
-            register(self.bolt_app)
+            register(self.bolt_app, self._runtime)
 
     def register_middlewares(self, *middleware_register: MiddlewareRegister):
         for register in middleware_register:
@@ -57,31 +65,37 @@ class SlackBotApp:
         # TODO: Init database schema when obsolete
         raise NotImplementedError("Database Initialization not yet implemented!")
 
+    # Start/Close mode for server-ful modes, e.g. local WS based testing
     def start(self, socket_mode=True):
         if socket_mode:
             self._socket_mode_handler = SocketModeHandler(self.bolt_app, self.config.slack_app_token)
             self._socket_mode_handler.start()
+        else:
+            raise NotImplementedError("SlackBotApp modes other than websocket not yet implemented")
 
     def close(self):
         if self._socket_mode_handler:
             self._socket_mode_handler.close()
         self.db.close()
 
+    def start_lambda(self, event, context):
+        self._lambda_mode_handler = SlackRequestHandler(self.bolt_app)
+        self._lambda_mode_handler.handle(event, context)
 
-# Start your app
-if __name__ == "__main__":
+
+def lambda_handler(event, context):
     _app_config = SlackBotConfig(
         slack_bot_token=os.environ.get("SLACK_BOT_TOKEN"),
-        slack_app_token=os.environ.get("SLACK_APP_TOKEN"),
-        debug=True,  # Turning on debug now
-        db_name=":memory:",  # Use memory for now
+        slack_signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+        db_name=os.environ.get("DB_NAME"),
+        db_hostname=os.environ.get("DB_HOSTNAME"),
+        db_username=os.environ.get("DB_USERNAME"),
+        db_password=os.environ.get("DB_PASSWORD"),
     )
-
     _bolt_app = SlackBoltApp(
         token=_app_config.slack_bot_token,
+        process_before_response=True,
     )
 
     _app = SlackBotApp(config=_app_config, bolt_app=_bolt_app)
-    _app.start()
-
-    atexit.register(_app.close)
+    _app.start_lambda(event, context)
--- bot/db/database.py
+++ bot/db/database.py
@@ -1,4 +1,5 @@
-from peewee import Database, DatabaseProxy, SqliteDatabase
+from peewee import Database, DatabaseProxy, SqliteDatabase, MySQLDatabase
+from playhouse.db_url import connect
 
 database_runtime = DatabaseProxy()
 
@@ -7,7 +8,7 @@ def use_database(db: Database):
     database_runtime.initialize(db)
 
 
-def new_sqlite_database(db_name: str) -> SqliteDatabase:
+def conn_sqlite_database(db_name: str) -> SqliteDatabase:
     return SqliteDatabase(
         db_name, pragmas={
             'journal_mode': 'wal',
@@ -16,3 +17,11 @@ def new_sqlite_database(db_name: str) -> SqliteDatabase:
             'ignore_check_constraints': 0,
             'synchronous': 0}
     )
+
+
+def conn_mysql_database(db_url: str) -> MySQLDatabase:
+    db = connect(db_url)
+
+    # Boldly assume connect will make Mysql connection
+    assert type(db) is MySQLDatabase
+    return db
